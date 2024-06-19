8aef45930e08d525a144b27d0400b7be35fb1d51
1thiagoCRUZ <thiagocruz58452@gmail.com>
2024-06-19 18:54:09 -0300

    Aula 5 - Aprendendo sobre as Props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
 
-  const name = 'Thiago'
-  const newName = name.toUpperCase()
-
-  function sum(a, b) {
-    return a + b
-  }
-
-  const url = "https://via.placeholder.com/150"
+  const name = "Messi"
   return (
     <div className="App">
-     <h2>Alterando o JSX</h2>
-     <p>Olá, {newName}</p>
-     <p>Soma: {sum(1,2)}</p> {/*Executando algumas funções do próprio JavaScript */}
-    <img src={url} alt='Minha Imagem'/>
-    <HelloWorld/>
+      <SayMyName nome="Thiago"/>
+      <SayMyName nome="Neymar"/>
+      <SayMyName nome={name}/>
+      <Pessoa 
+      nome="Thiago" 
+      idade="18" 
+      profissao="Analista" 
+      foto="https://via.placeholder.com/150"/>
     </div>
   );
 }
