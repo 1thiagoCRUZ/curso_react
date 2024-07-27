9119c26d9c36962f04ec1fdeea7aaaae13f33892
1thiagoCRUZ <thiagocruz58452@gmail.com>
2024-07-27 18:43:17 -0300

    Adicionando CSS e aprendendo sobre CSS modules

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 function App() {
 
   const name = "Messi"
   return (
     <div className="App">
+      <Frase />
+      <Frase />
       <SayMyName nome="Thiago"/>
       <SayMyName nome="Neymar"/>
       <SayMyName nome={name}/>
