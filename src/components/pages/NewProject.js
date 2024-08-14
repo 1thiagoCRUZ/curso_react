a4a6efe77c90c6d90cefca130a4a3770362a0edc
1thiagoCRUZ <thiagocruz58452@gmail.com>
2024-08-14 19:40:31 -0300

    Aula 24 - Inserindo dados no banco via API - 10:30

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,15 +1,46 @@
+import { useHistory } from 'react-router-dom'
 import styles from './NewProject.module.css'
 import ProjectForm from '../project/ProjectForm'
 
 
 function NewProjetct() {
-    return (
-      <div className={styles.newproject_container}>
-        <h1> Criar Projeto</h1>
-        <p> Crie seu projeto para depois adicionar os serviços</p>
-        <ProjectForm  btnText="Criar Projeto"/>
-      </div>
-    )
+
+  /* Esse hook permite redirecionar o usuário quando for necessário
+  Nesse caso quando o usuário fizer uma requisição POST podemos redirecioná-lo
+  para outra página */
+
+  const history = useHistory()
+
+  // Função responsável por inserir o projeto no sistema
+  function createPost(project) {
+
+    // initialize cost and services
+    project.cost = 0
+    project.services = []
+
+    fetch('http://localhost:5000/projects', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(project)
+    }).then(
+      ((resp) => resp.json())
+        .then((data) => {
+          console.log(data)
+
+          // espaço para o redirect
+        })
+    ).catch(err => console.log(err))
   }
-  
+
+  return (
+    <div className={styles.newproject_container}>
+      <h1> Criar Projeto</h1>
+      <p> Crie seu projeto para depois adicionar os serviços</p>
+      <ProjectForm handleSubmit={createPost} btnText="Criar Projeto" />
+    </div>
+  )
+}
+
 export default NewProjetct
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -4,10 +4,12 @@ import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 import styles from './Project.module.css'
+import { PiRocket } from 'react-icons/pi'
 
-function ProjectForm({ btnText }) {
+function ProjectForm({ handleSubmit, btnText, projectData }) {
     // Vai começar como um array vazio esperando a resposta que vai vir da API
     const [categories, setCategories] = useState([])
+    const [project, setProject] = useState(projectData || []) // Vai receber os dados do projeto ou um objeto vazio
 
     useEffect(() => {
 
@@ -25,12 +27,38 @@ function ProjectForm({ btnText }) {
         // o catch vai imprimir os possíveis erros que podem dar na nossa request
     }, [])
 
-    return (
-        <form className={styles.form}>
-            <Input type="text" text="Nome do projeto" name="name" placeholder="Insira o nome do projeto" />
+    const submit = (e) => {
+        e.preventDefault()
+        handleSubmit(project)
+    }
 
-            <Input type="number" text="Orçamento do projeto" name="budget" placeholder="Insira o orçamento total" />
-            <Select name="category_id" text="Selecione a categoria" options={categories} />
+    function handleChange(e) {
+        setProject({ ...project, [e.target.name]: e.target.value })
+        console.log(project)
+    }
+
+    return (
+        <form onSubmit={submit} className={styles.form}>
+            <Input 
+            type="text" 
+            text="Nome do projeto" 
+            name="name" 
+            placeholder="Insira o nome do projeto"
+            handleOnChange={handleChange} 
+            />
+
+            <Input 
+            type="number" 
+            text="Orçamento do projeto" 
+            name="budget" 
+            placeholder="Insira o orçamento total"
+            handleOnChange={handleChange} 
+             />
+
+            <Select 
+            name="category_id" 
+            text="Selecione a categoria" 
+            options={categories} />
 
             <SubmitButton text={btnText} />
         </form>
