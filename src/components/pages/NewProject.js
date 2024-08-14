b1b53c21e952b838d1cc84d3dc899b904b4a7f35
1thiagoCRUZ <thiagocruz58452@gmal.com>
2024-08-14 17:28:37 -0300

    Estilizando e componentizando o nosso formulário de criação de projeto

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -7,7 +7,7 @@ function NewProjetct() {
       <div className={styles.newproject_container}>
         <h1> Criar Projeto</h1>
         <p> Crie seu projeto para depois adicionar os serviços</p>
-        <ProjectForm />
+        <ProjectForm  btnText="Criar Projeto"/>
       </div>
     )
   }
