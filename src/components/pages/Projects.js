4255301f4282ac5d4e2833b2c66c3a75e3a1b19d
1thiagoCRUZ <thiagocruz58452@gmail.com>
2024-08-18 21:38:39 -0300

    Aula 25 | Mensagens do Sistema (Projeto criado com sucesso)

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,32 @@
+import { useLocation } from "react-router-dom" // hook //
+
+import Message from "../layout/Message"
+
 function Projects() {
-    return <h1>Página de Projeto</h1>  
+  /* Constante que vai ser o useLocation */
+  const location = useLocation()
+
+  /* Declaramos a variável de message, inicialmente vazia */
+  let message = ''
+
+  /* Se possuir algum conteúdo no state location eu vou ver se a message existe
+  e ai eu vou acessar message que é o location.state.message. Veio o location state?  
+  Então eu vou acessar o message que eu sei que tá lá e eu vou atribuir o valor a variável
+  message */
+  if(location.state) {
+    message = location.state.message
+  }
+
+    return (
+      <div>
+        <h1>Meus Projetos</h1>
+        {/* Se o messagem ta preenchido então vamos imprimir o componente de Mensagem.
+        Passamos o type succes porque sabemos que quando ele vem desse jeito sempre 
+        vai ser sucesso */}
+        {message && <Message type="success" msg={message} />}
+     
+      </div>
+    )
   }
   
   export default Projects
